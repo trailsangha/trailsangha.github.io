209ef17d04f1cbf1033c36ef1a397f3fa2a22ec1
trailsangha <111054228+trailsangha@users.noreply.github.com>
2022-10-26 07:05:08 -0400

    Update entrypoint.sh

--- .github/actions/libreoffice/entrypoint.sh
+++ .github/actions/libreoffice/entrypoint.sh
@@ -4,6 +4,24 @@
 # Boilerplate to run parameter 1 as command.
 #
 
+
+# OLD FORMAT:
+#
+# - name: Set output
+#  run: echo "::set-output name={name}::{value}"
+#
+# NEW FORMAT:
+#
+# - name: Set output
+#   run: echo "{name}={value}" >> $GITHUB_OUTPUT
+#
+
+# OLD CODE:
+# sh -c "$1"
+# time=$(date)
+# echo "::set-output name=time::$time"
+
+# NEW CODE:
 sh -c "$1"
 time=$(date)
-echo "::set-output name=time::$time"
+echo "time=$time" >> $GITHUB_OUTPUT
